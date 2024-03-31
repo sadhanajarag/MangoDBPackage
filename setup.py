92cf1da2c68843487c125a8500f6c6f37105a352
sadhanajarag <sadhana20.jarag@gmail.com>
2024-04-01 02:12:25 +0530

    code and version updated_3

--- setup.py
+++ setup.py
@@ -1,18 +1,6 @@
 from setuptools import setup, find_packages
 from typing import List
 
-HYPEN_E_DOT = '-e .'
-def get_requirements (file_path:str)->List[str]:
-    requirements = []
-    with open(file_path) as f:
-        requirements = f.readline()
-        requirements = [req.replace("\n","")for req in requirements]
-
-        if HYPEN_E_DOT  in requirements:
-            requirements.remove(HYPEN_E_DOT)
-        return requirements
-
-
 with open('README.md', 'r', encoding='utf-8') as f:
     long_description = f.read()     
    
@@ -22,6 +10,7 @@ REPO_NAME = "mongodbconnectorpkg"
 PKG_NAME= "databaseautomation"
 AUTHOR_USER_NAME = "sadhanajarag"
 AUTHOR_EMAIL = "sadhana20.jarag@gmail.com"
+
 setup(
     name=PKG_NAME,
     version=__version__,
@@ -35,6 +24,4 @@ setup(
         "Bug Tracker": f"https://github.com/{AUTHOR_USER_NAME}/{REPO_NAME}/issues",
     },
     package_dir={"": "src"},
-    packages=find_packages(where="src"),
-    install_requires=get_requirements("./requirements_dev.txt")
-    )
+    packages=find_packages(where="src"),)
